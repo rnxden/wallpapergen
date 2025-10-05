e8521d8546722e3f16f62d2d1ae627dfca5eef4a
Arden X <rnx@rnx.sh>
2025-10-05 01:47:54 -0400

    refactor: describe and document iphone wallpaper generator

--- main.py
+++ main.py
@@ -3,32 +3,44 @@ from math import sqrt
 from PIL import Image, ImageDraw
 
 
-def main():
-    w = 1179
-    h = 2556
+def iphone15():
+    width = 1179
+    height = 2556
 
-    im = Image.new("RGB", (w, h), (0, 0, 0))
-    d = ImageDraw.Draw(im)
+    image = Image.new("RGB", (width, height), (0, 0, 0))
+    draw = ImageDraw.Draw(image)
 
-    cx = 23
-    cy = 50
-    for i in range(1, cx):
-        for j in range(1, cy):
-            x = i * w / cx
-            y = j * h / cy
-            r = 2
+    dots_x = 23
+    dots_y = 50
 
-            rel_x = w / 2 - x
-            rel_y = h / 2 - y
+    for i in range(1, dots_x):
+        for j in range(1, dots_y):
+            x = i * width / dots_x
+            y = j * height / dots_y
+
+            rel_x = width / 2 - x
+            rel_y = height / 2 - y
+
             off_x = 0
             off_y = 100
+            rad_x = 1750
+            rad_y = 1250
 
-            dist = sqrt(((rel_x + off_x) / 1750) ** 2 + ((rel_y + off_y) / 1250) ** 2)
-            c = int(90 * max(0, 1 - dist))
+            # This is the ellipse formula. The goal is to center and offset the ellipse, then
+            # calculate how far the point is from the center of the ellipse, using the increasing
+            # distance as the dot fade color.
+            dist = sqrt(((rel_x + off_x) / rad_x) ** 2 + ((rel_y + off_y) / rad_y) ** 2)
 
-            d.circle((x, y), radius=r, fill=(c, c, c))
+            dot_rad = 2
+            dot_col = int(90 * max(0, 1 - dist))
 
-    im.save("out.png")
+            draw.circle((x, y), radius=dot_rad, fill=(dot_col, dot_col, dot_col))
+
+    image.save("iphone15.png")
+
+
+def main():
+    iphone15()
 
 
 if __name__ == "__main__":
